d62a1446419c40900a90972c5a5dbd8c47979e0c
Davaajargal <53143013+Davaajargal-M@users.noreply.github.com>
2021-05-06 19:51:12 +0800

    string length

---
+++ HW-1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+using namespace std; 
+
+int main(){
+	string text = "Easy and fast service, very accurate and powerful. I have been looking for a quick solution to check my IELTS essays for few weeks now.";
+	
+	cout<<text.length();
+	
+	return 0; 
+}
+
