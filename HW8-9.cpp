bdec833da35b1b35ad679497ea21702d9319f4af
Davaajargal <53143013+Davaajargal-M@users.noreply.github.com>
2021-05-22 15:59:37 +0800

    Total words of a text

--- HW8-9.cpp
+++ HW8-9.cpp
@@ -4,19 +4,15 @@
 using namespace std; 
 
 int main(){
-	string text = "Easy and fast service, very accurate and powerful.";
-	int vowels = 0, consonants = 0; 
+	string text = "The code includes definitions for 128 characters: most of these are the printable characters of the alphabet such as abc, ABC, 123, and ?&!.";
+	int words = 0;
 	
-	for(int i =0; i<=text.length(); i++){
-		if(text[i]>='a' && text[i]<='z' ||text[i]>='A' && text[i]<='Z'){
-			if(text[i]=='a' || text[i]=='A' || text[i]=='e' || text[i]=='E' || text[i]=='o' || text[i]=='O' || text[i]=='u' || text[i]=='U' || text[i]=='y' || text[i]=='Y' || text[i]=='i' || text[i]=='I'){
-				vowels++;
-			}else{
-				consonants++;	
-			}
+	for(int i = 0; i<=text.length(); i++){
+		if(text[i]==' ' || text[i]=='\0'){
+			words++;
 		}
 	}
-	cout<<"Total vowels: "<<vowels<<endl<<"Total consonants: "<<consonants;
+	cout<<"Total words of this text: "<<words;
 	
 	
 	return 0; 
